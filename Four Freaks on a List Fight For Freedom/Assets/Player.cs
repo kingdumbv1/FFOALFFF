09ebcf9184f89579e03e57fa00c93af87b6ad76d
kingdumbv1 <derpofth3dope@gmail.com>
2025-02-28 00:36:53 -0800

    prototype 1/2

--- Four Freaks on a List Fight For Freedom/Assets/Player.cs
+++ Four Freaks on a List Fight For Freedom/Assets/Player.cs
@@ -3,17 +3,34 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Vector2 currentDirection;
+    [SerializeField] float isAttacking;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+
+        if (currentDirection.y >= 0f && isAttacking == 1f)
+        {
+
+        }
+
+        rb.linearVelocityX = currentDirection.x * moveSpeed;
     }
 
     public void TestInput(InputAction.CallbackContext context)
     {
         Debug.Log(context.ReadValue<Vector2>());
+        currentDirection = context.ReadValue<Vector2>();
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        isAttacking = context.ReadValue<float>();
     }
 }
